23fab7b8b028fcf19263751c53c9f35107ce720a
Vincent Tanujaya <vincent.tanujaya@gmail.com>
2019-11-11 08:05:53 +0700

    init

---
+++ apps/modules/dashboard/controllers/web/NoteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Phalcon\Init\Dashboard\Controllers\Web;
+
+use Phalcon\Mvc\Controller;
+
+class NoteController extends Controller
+{
+    public function indexAction()
+    {
+        echo ("hahaha");
+    }
+
+    public function randomAction()
+    {
+        echo ("random");
+    }
+}
